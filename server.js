6dd91a8b8b9d1e99465765c54a23556bbcd0b244
dem0saic <changeinculture4lyf@proton.me>
2023-12-18 09:15:06 +0000

    Define the Patient schema start

--- server.js
+++ server.js
@@ -9,3 +9,23 @@ mongoose.connect('mongodb://localhost/medical_records', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+.then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB', error);
+  });
+
+  // Define the Patient schema start
+const patientSchema = new mongoose.Schema({
+    patientId: { type: String, required: true },
+    surname: { type: String, required: true },
+    otherNames: { type: String, required: true },
+    gender: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    address: { type: String, required: true },
+    emergencyName: { type: String, required: true },
+    emergencyContact: { type: String, required: true },
+    relationship: { type: String, required: true },
+  });
+   // Define the Patient schema ends
